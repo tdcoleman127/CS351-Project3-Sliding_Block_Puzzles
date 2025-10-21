619594657e5faf3a06cd416d80f237a8b068b048
tdcoleman127 <figgeryfirehead@gmail.com>
2025-10-20 20:09:29 -0500

    Building up Grid class with tests for input, making sure overlapping Pieces can be calculated and pieces don't fall outside of Grid

--- proj3starter.py
+++ proj3starter.py
@@ -44,54 +44,99 @@ class Grid:
         self.colLimit = colLimit
 
     def __str__(self):
-        return "This grid consists of " + str(len(self.pieces)) + " across " + self.rowLimit + "rows and " + self.colLimit + "columns"
+        return "This grid consists of " + str(len(self.pieces)) + " pieces across " + str(self.rowLimit) + " rows and " + str(self.colLimit) + " columns"
 
     def pieceNames(self):
-        pass
+        print("Pieces in this grid:")
+        for p in self.pieces:
+            print(p)
+
+    def pieceOverlapping(self, r, c) -> bool:
+        for p in self.pieces:
+            if(r == p.rowPos and c == p.colPos):
+                return True
+        return False
+        
     
 # print("Piece " + Piece.name + " moves " + Movement.distance + "spaces " + direction)
         
 def slidingBlock(filename):
     # 4  4
-    text = "3  1  2  1  b"
-    # 1  1  1  1  b
+    # text = "3  1  2  1  b"
+    # # 1  1  1  1  b
 
-    details = text.split()
-    # details2 = ['3', '2', '1', '1', 'h']
-    # details3 = ['3', '2', '1', '1', 'h']
+    # details = text.split()
+    # # details2 = ['3', '2', '1', '1', 'h']
+    # # details3 = ['3', '2', '1', '1', 'h']
 
-    myPiece = Piece('Z', int(details[0]), int(details[1]), int(details[2]),
-          int(details[3]), details[4])
-    piece2 = Piece()
+    # testPiece = Piece('Z', int(details[0]), int(details[1]), int(details[2]),
+    #       int(details[3]), details[4])
+    # blankPiece = Piece()
     
-    print(myPiece)
-    print(piece2)
+    # # print(testPiece)
+    # # print(blankPiece)
 
-    # Piece name can be checked
-    if(myPiece.name == 'Z'):
-        print("ROCK THE DRAGON")
+    # testGrid = Grid(4, 4)
+    # testGrid.pieces = [testPiece, blankPiece]
+    # print(testGrid)
+    # print(testGrid.pieceNames())
+    # print(testGrid.pieceOverlapping(3, 1))
+    # print(testGrid.pieceOverlapping(10, 1))
+    # print(testGrid.pieceOverlapping(0, 0))
+    # print(testGrid.pieceOverlapping(0, 4))
+
+
+    # # Piece name can be checked
+    # if(testPiece.name == 'Z'):
+    #     print("ROCK THE DRAGON")
 
     print ("Sliding Block Puzzle Solver")
     print ("using data in file:", filename)
 
     # Logic for inserting pieces into the grid from file input
-    myGrid = None
-    myPiece = None
-    for line in file:
-        details = line.split()
-        if len(details) == 2:
-            myGrid = Grid(details[0], details[1])
-            print("Error: Number of rows must be greater than zero")
-            print("Error: Number of columns must be greater than zero")
+
+    # Intializing puzzle grid and each piece before its inserted
+    myGrid = Grid()
+    myPiece = Piece()
+
+    # Reading in each line in file
+    details2 = ["5  5", "3  1  2  1  b", "4  3  1  1  b", "1  1  2  1  b", "3  1  2  1  b",
+                "0  1  2  1  b", "-1  -83  2  1  b", "0  7  2  1  b"]
+    for item in details2:
+        gameInput = item.split()
+        if len(gameInput) == 2:
+            if(int(gameInput[0]) <= 0):
+                print("Error: Number of rows must be greater than zero")
+                return
+            elif(int(gameInput[1]) <= 0):
+                print("Error: Number of columns must be greater than zero")
+                return
+            else:
+                myGrid.rowLimit = int(gameInput[0])
+                myGrid.colLimit = int(gameInput[1])
         else:
 
-            if(details[0] > myGrid.rowLimit or details[1] > myGrid.colLimit):
+            if(int(gameInput[0]) > myGrid.rowLimit or int(gameInput[1]) > myGrid.colLimit
+               or int(gameInput[0]) <= 0 or int(gameInput[1]) <= 0):
                 # Handle piece falling outside of grid
                 print("Warning: Piece with starting position of R,C falls outside of grid")
-            myPiece = Piece('Z', int(details[0]), int(details[1]), int(details[2]),
-          int(details[3]), details[4])
-            # Add this piece to the Grid
-            Grid.pieces.append(myPiece)
+
+            elif( myGrid.pieceOverlapping(int(gameInput[0]), int(gameInput[1])) == True ):
+                # Piece overlaps with a created piece 
+                print("Warning: Piece with starting position of R,C overlaps with other piece")
+
+            else:
+                # Average case: Add this piece to the Grid
+                myPiece = Piece('Z', int(gameInput[0]), int(gameInput[1]), int(gameInput[2]), 
+                                int(gameInput[3]), gameInput[4])
+                myGrid.pieces.append(myPiece)
+
+            # elif():
+            #     # Handle invalid direction of movement
+            #     print("Warning: Piece with starting position of R,C has invalid movement")
+    print(myGrid)
+    print(myGrid.pieceNames())
+
     pass
 
 
